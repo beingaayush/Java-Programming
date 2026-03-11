8fd0a14b5f602802a2ec592a8d4d632876f29388
beingaayush <beingaayushraj@gmail.com>
2026-03-11 19:59:47 +0530

    loops

---
+++ Loops/For.java
@@ -0,0 +1,15 @@
+class For{
+    public static void main(String args[])
+    {
+        for(int i=1; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+//output:
+// 1
+// 2
+// 3
+// 4
+// 5

9ad7c9bd2cd39ed32f3ce24140fac6555bc21f3b
beingaayush <beingaayushraj@gmail.com>
2026-03-12 16:40:04 +0530

    array

---
+++ Array/Ex1.java
@@ -0,0 +1,17 @@
+// taking array input through user
+
+import java.util.*;
+class Ex1{
+    public static void main(String args[]){
+        Scanner = new Scanner(System.in);
+
+        System.out.println("Enter Size of an array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        System.out.println("Enter Elements: ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+    }
+}
---
+++ Array/Ex2.java
@@ -0,0 +1,12 @@
+// Array Traversal
+
+import java.util.*;
+class Ex2{
+    public static void main(String args[]){
+        int arr[] = {10, 20, 30, 40};
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}

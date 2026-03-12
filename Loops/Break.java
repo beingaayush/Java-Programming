07854728c5a25aea4b96cb163caea5d454339dcb
beingaayush <beingaayushraj@gmail.com>
2026-03-12 15:32:33 +0530

    Loops - Break in java

---
+++ Loops/Break.java
@@ -0,0 +1,20 @@
+class Break{
+    public static voi dmain(String args[])
+    {
+        for(int i=1; i <= 10; i++){
+            if(i === 5) break;
+            System.out.println(i);
+        }
+    }
+}
+
+
+// Output
+
+// 1
+// 2
+// 3
+// 4
+
+// Explanation:
+// i = 5 pe loop terminate ho gaya.

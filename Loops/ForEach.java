b885a2007ee45c85a549bc125dfe5456619b37d8
beingaayush <beingaayushraj@gmail.com>
2026-03-12 15:38:48 +0530

    for - each loop

---
+++ Loops/ForEach.java
@@ -0,0 +1,18 @@
+class ForEach{
+    public static void main(String args[])
+    {
+        int arr[] = {10, 20, 30, 40};
+        for(int num: arr){
+            System.out.println(num);
+        }
+    }
+}
+
+
+
+// Output
+
+// 10
+// 20
+// 30
+// 40

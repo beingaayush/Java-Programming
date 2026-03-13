67d6003832ca66df65a2678dee163fbf932b4fe6
beingaayush <beingaayushraj@gmail.com>
2026-03-13 18:09:35 +0530

    Encapsulation done

---
+++ Phase 3 - OOP &  Java packages/Encapsulation.java
@@ -0,0 +1,32 @@
+// Encapsulation in Java
+// Encapsulation = data ko hide karna + controlled access dena.
+
+// Iska basic rule:
+// variables → private
+// access → public methods (getter / setter)
+
+class Car {
+
+    private int speed;
+    //setter
+    public void setSpeed(int s){
+        if(s > 0){
+            speed = s;
+        }
+    }
+    //getter
+    public int getSpeed(){
+        return speed;
+    }
+
+}
+public class Encapsulation {
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        c.setSpeed(100);
+
+        System.out.println(c.getSpeed());
+    }
+}

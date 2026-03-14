595ede3c0ef3dc2a9b49bd2cd4b7ab1dc661440b
beingaayush <beingaayushraj@gmail.com>
2026-03-14 23:26:25 +0530

    Interface and rules of interface

---
+++ Phase 3 - OOP &  Java packages/Interface.java
@@ -0,0 +1,29 @@
+// Interface :- Interface = pure abstraction.
+// Matlab: sirf method declaration implementation nahi
+
+// rules:
+// Interface ka object create nahi kar sakte
+// Methods by default public abstract hote hain
+// Variables by default public static final hote hain
+// Class interface ko implements karti hai
+// Interface methods implement karna mandatory hota hai
+// Ek class multiple interfaces implement kar sakti hai
+// Interface me constructor nahi hota
+--------------------------------------------------------------------------------------------------
+
+interface Animal {
+    void sound();
+}
+
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}

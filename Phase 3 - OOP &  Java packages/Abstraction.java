f192a0ec19d680925210e9fc3ae92613585850d6
beingaayush <beingaayushraj@gmail.com>
2026-03-14 23:04:22 +0530

    abstraction notes

---
+++ Phase 3 - OOP &  Java packages/Abstraction.java
@@ -0,0 +1,9 @@
+// Abstraction in Java :-
+// Abstraction = unnecessary details hide karna aur sirf important functionality dikhana.
+
+// Real meaning:
+// WHAT the object does not HOW it does it
+
+// Example real world:- ATM machine
+// Tum withdraw() use karte ho,
+// lekin andar bank server kaise kaam kar raha hai wo nahi dekhte.
